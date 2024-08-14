c04893c4af25841a266730d268be65ede9a95ac2
florian <florian.swienty@gmail.com>
2024-08-14 19:07:21 +0200

    updated to svg icons

--- scripts/autoload/globals.gd
+++ scripts/autoload/globals.gd
@@ -7,12 +7,13 @@ const menu_transition_time: float = 0.3
 
 var player_infos: Dictionary = {}
 
+# should be numbered consecutively starting at 0
 const ai_levels: Dictionary = {
-    1: {name = "Amateur", incompetence = 7.0},
-    2: {name = "Normal", incompetence = 3.0},
-    3: {name = "Pro", incompetence = 0.1},
+    0: {name = "Amateur", incompetence = 7.0},
+    1: {name = "Normal", incompetence = 3.0},
+    2: {name = "Pro", incompetence = 0.1},
 }
-var current_ai_level: int = 2
+var current_ai_level: int = 1
 
 var queue_mode = Enums.QueueControl.DRAG
 
--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -8,36 +8,53 @@ var lobby_code_error_anim: AnimationPlayer
 var drag_tab_title: String = "Drag"
 var scroll_wheel_tab_title: String = "Scroll Wheel"
 
-@onready var play_button: Button = $"%PlayButton"
-@onready var quit_button: Button = $"%QuitButton"
-@onready var increase_diffuculty_button: Button = $"%IncreaseDifficultyButton"
-@onready var decrease_diffuculty_button: Button = $"%DecreaseDifficultyButton"
-@onready var difficulty_label: Label = $"%AiDifficultyLabel"
+@onready var play_button: Button = %PlayButton
+@onready var quit_button: Button = %QuitButton
+@onready var increase_diffuculty_button: Button = %IncreaseDifficultyButton
+@onready var decrease_diffuculty_button: Button = %DecreaseDifficultyButton
+@onready var difficulty_label: Label = %AiDifficultyLabel
+@onready var difficulty_option_button: OptionButton = %DifficultyOptionButton
 
 
 func _ready():
 	play_button.connect("pressed", _on_PlayButton_pressed)
 	increase_diffuculty_button.connect("pressed", _on_IncreaseDifficultyButton_pressed)
 	decrease_diffuculty_button.connect("pressed", _on_DecreaseDifficultyButton_pressed)
+	_init_difficulty_option_button()
 	quit_button.connect("pressed", _on_QuitButton_pressed)
 	modulate = Color.TRANSPARENT
 	_set_buttons_enabled(true)
+
+func _init_difficulty_option_button():
+	difficulty_option_button.connect("pressed", func(): SoundManager.click())
+	difficulty_option_button.connect("item_selected", _on_diff_eeh)
+
+	for ai_level in Globals.ai_levels:
+		difficulty_option_button.add_item(Globals.ai_levels[ai_level]["name"], ai_level)
+	difficulty_option_button.select(Globals.current_ai_level)
+	difficulty_option_button.text = "Opponent: " + Globals.ai_levels[Globals.current_ai_level]["name"]
+
+func _on_diff_eeh(idx):
+	SoundManager.click()
+	Globals.current_ai_level = idx
+	difficulty_option_button.text = "Opponent: " + Globals.ai_levels[Globals.current_ai_level]["name"]
 
 
 func _update_difficulty():
 	difficulty_label.text = Globals.ai_levels[Globals.current_ai_level]["name"]
 	print(Globals.current_ai_level)
-	if Globals.current_ai_level == 1:
+	if Globals.current_ai_level == 0:
 		decrease_diffuculty_button.disabled = true
 	else:
 		decrease_diffuculty_button.disabled = false
-	if Globals.current_ai_level == Globals.ai_levels.size():
+	if Globals.current_ai_level == Globals.ai_levels.size() - 1:
 		increase_diffuculty_button.disabled = true
 	else:
 		increase_diffuculty_button.disabled = false
 
 
 func _set_buttons_enabled(enabled: bool):
+	# TODO do this with element that catches clicks
 	play_button.disabled = not enabled
 	increase_diffuculty_button.disabled = not enabled
 	decrease_diffuculty_button.disabled = not enabled
@@ -52,14 +69,14 @@ func _on_PlayButton_pressed():
 
 func _on_IncreaseDifficultyButton_pressed():
 	SoundManager.click()
-	if Globals.current_ai_level < Globals.ai_levels.size():
+	if Globals.current_ai_level < Globals.ai_levels.size() - 1:
 		Globals.current_ai_level += 1
 	_update_difficulty()
 
 
 func _on_DecreaseDifficultyButton_pressed():
 	SoundManager.click()
-	if Globals.current_ai_level > 1:
+	if Globals.current_ai_level > 0:
 		Globals.current_ai_level -= 1
 	_update_difficulty()
 
