e95090672cbc08b5bd39221d710037d0405e4be2
florian <florian.swienty@gmail.com>
2024-08-16 00:20:32 +0200

    put license text directly in source code

--- scripts/menus/licences.gd
+++ scripts/menus/licences.gd
@@ -5,9 +5,7 @@ func _ready():
 	self.text = ""
 
 	# add tabler license
-	self.append_text("[center][b]Tabler Icons[/b][/center]\n")
-	var tabler_text = load_from_file("res://assets/licenses/tabler.txt")
-	self.append_text(tabler_text)
+	_add_tabler_license()
 
 	# add all other licenses
 	var license_infos = Engine.get_license_info()
@@ -43,7 +41,29 @@ func _license_type_to_array(licenses: String) -> Array[String]:
 	licenses_array = licenses_array.filter(func(l): return l != "public-domain")
 	return licenses_array
 
-func load_from_file(path):
-	var file = FileAccess.open(path, FileAccess.READ)
-	var content = file.get_as_text()
-	return content
+
+func _add_tabler_license():
+	self.append_text("[center][b]Tabler Icons[/b][/center]\n")
+	var tabler_text = """MIT License
+
+Copyright (c) 2020-2024 Paweł Kuna
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
+	self.append_text(tabler_text)
