9514a0c46479923259037e705e1b18ab374ad6b2
florian <florian.swienty@gmail.com>
2024-08-15 18:48:33 +0200

    added some licenses

--- scripts/menus/licences.gd
+++ scripts/menus/licences.gd
@@ -2,8 +2,20 @@ extends RichTextLabel
 
 
 func _ready():
-	self.text = Engine.get_license_text()
+	self.append_text("[center][b]Godot Engine[/b][/center]\n")
+	self.append_text(Engine.get_license_text())
+	
+	self.append_text("\n[center][b]Tabler Icons[/b][/center]\n")
+	var tabler_text = load_from_file("res://assets/licenses/tabler.txt")
+	self.append_text(tabler_text)
+
 	for license_info in Engine.get_license_info():
 		print(license_info)
 	for copyright_info in Engine.get_copyright_info():
 		print(copyright_info)
+
+
+func load_from_file(path):
+	var file = FileAccess.open(path, FileAccess.READ)
+	var content = file.get_as_text()
+	return content
